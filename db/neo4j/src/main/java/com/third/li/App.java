5875e0df0a510c1ca6a8da2cf3f4df4c817f12ff
jamielee <jamielee.studio@gmail.com>
2026-06-01 23:22:16 +0800

    feat: add db module

---
+++ db/neo4j/src/main/java/com/third/li/App.java
@@ -0,0 +1,13 @@
+package com.third.li;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
